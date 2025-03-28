40af0ac38408fc67e0d05ec1f63e65ee2f167c9b
Taylor <78652522+Taylor-eOS@users.noreply.github.com>
2025-03-28 09:57:25 +0100

    Example from model page

---
+++ trillion.py
@@ -0,0 +1,35 @@
+import torch
+from transformers import AutoModelForCausalLM, AutoTokenizer
+
+model_name = "trillionlabs/Trillion-7B-preview"
+
+model = AutoModelForCausalLM.from_pretrained(
+    model_name,
+    torch_dtype=torch.bfloat16,
+    device_map="auto"
+)
+tokenizer = AutoTokenizer.from_pretrained(model_name)
+
+prompt = input("Prompt: ")
+messages = [
+    {"role": "user", "content": prompt}
+]
+text = tokenizer.apply_chat_template(
+    messages,
+    tokenize=False,
+    add_generation_prompt=True
+)
+model_inputs = tokenizer([text], return_tensors="pt").to(model.device)
+
+generated_ids = model.generate(
+    model_inputs["input_ids"],
+    attention_mask=model_inputs["attention_mask"],
+    max_new_tokens=512
+)
+generated_ids = [
+    output_ids[len(input_ids):] for input_ids, output_ids in zip(model_inputs.input_ids, generated_ids)
+]
+
+response = tokenizer.batch_decode(generated_ids, skip_special_tokens=True)[0]
+print(response)
+
