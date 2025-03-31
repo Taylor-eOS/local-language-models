74fe14ee11559cef5e91dee28dd8949bde3b46e1
Taylor <78652522+Taylor-eOS@users.noreply.github.com>
2025-03-31 22:30:04 +0200

    End generation properly

--- trillion.py
+++ trillion.py
@@ -1,33 +1,37 @@
 import torch
 from transformers import AutoModelForCausalLM, AutoTokenizer, TextStreamer
 
-model_name = "trillionlabs/Trillion-7B-preview"
-
 model = AutoModelForCausalLM.from_pretrained(
-    model_name,
+    "trillionlabs/Trillion-7B-preview",
     torch_dtype=torch.bfloat16,
     device_map="auto"
 )
-tokenizer = AutoTokenizer.from_pretrained(model_name)
+tokenizer = AutoTokenizer.from_pretrained("trillionlabs/Trillion-7B-preview")
 
 while True:
-    prompt = input("\nPrompt: ")
-    messages = [{"role": "user", "content": prompt}]
-    text = tokenizer.apply_chat_template(
-        messages,
-        tokenize=False,
-        add_generation_prompt=True
-    )
-    model_inputs = tokenizer([text], return_tensors="pt").to(model.device)
-    streamer = TextStreamer(
-        tokenizer,
-        skip_prompt=True,
-        skip_special_tokens=True
-    )
-    _ = model.generate(
-        model_inputs["input_ids"],
-        attention_mask=model_inputs["attention_mask"],
-        max_new_tokens=512,
-        streamer=streamer,
-    )
+    try:
+        prompt = input("\nYou: ").strip()
+        if not prompt:
+            continue
+        messages = [{"role": "user", "content": prompt}]
+        formatted_prompt = tokenizer.apply_chat_template(
+            messages,
+            tokenize=False,
+            add_generation_prompt=True)
+        
+        inputs = tokenizer(formatted_prompt, return_tensors="pt").to(model.device)
+        if "token_type_ids" in inputs:
+            del inputs["token_type_ids"]
+        
+        streamer = TextStreamer(tokenizer, skip_prompt=True)
+        _ = model.generate(
+            **inputs,
+            max_new_tokens=512,
+            streamer=streamer,
+            eos_token_id=tokenizer.eos_token_id,
+            pad_token_id=tokenizer.eos_token_id)
+        
+    except KeyboardInterrupt:
+        print("\nGoodbye!")
+        break
 
